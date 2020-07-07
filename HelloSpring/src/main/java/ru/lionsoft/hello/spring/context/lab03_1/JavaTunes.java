fd029ef2486ec3b1381493d67aa9fd3fa6f6bf08
Igor Morenko <morenko@lionsoft.ru>
2020-07-07 15:18:48 +0300

    Добавлен пример с шаблоном поиска подпапок. Добавлены константы для конфигурационных файлов applicationContext.xml^ inMemoryItemDAO.xml

--- HelloSpring/src/main/java/ru/lionsoft/hello/spring/context/lab03_1/JavaTunes.java
+++ HelloSpring/src/main/java/ru/lionsoft/hello/spring/context/lab03_1/JavaTunes.java
@@ -10,8 +10,12 @@ package ru.lionsoft.hello.spring.context.lab03_1;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.lionsoft.hello.spring.context.entity.MusicItem;
+import static ru.lionsoft.hello.spring.util.PathUtil.path;
 
 public class JavaTunes {
+
+    private static final String APPLICATION_CONTEXT = path(JavaTunes.class, "applicationContext.xml");
+    private static final String IN_MEMORY_ITEM_DAO = path(JavaTunes.class, "inMemoryItemDAO.xml");
 
     public static void main(String[] args) {
         System.out.println("We're ready to learn Spring");
@@ -19,12 +23,13 @@ public class JavaTunes {
         // Create an ApplicationContext that reads the multiple files, using various capabilities
         // Use wildcards
         // ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:ru/lionsoft/hello/spring/context/lab03_1*.xml");
+        
         // Use file: - note this will be relative to the root directory of the project, which is where it is run from
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("file:target/classes/ru/lionsoft/hello/spring/context/lab03_1/*.xml");
+        // ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("file:target/classes/ru/lionsoft/hello/spring/context/lab03_1/*.xml");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("file:target/classes/**/lab03_1/*.xml");
 
         // List the two files directly
-        // ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("ru/lionsoft/hello/spring/context/lab03_1/inMemoryItemDAO.xml", 
-        //        "ru/lionsoft/hello/spring/context/lab03_1/applicationContext.xml");
+        // ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT, IN_MEMORY_ITEM_DAO);
         
         // Look up the javaTunesCatalog bean 
         Catalog cat = (Catalog) ctx.getBean("javaTunesCatalog");
